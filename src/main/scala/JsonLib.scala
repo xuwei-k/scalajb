6f3b79cd3a11a4e65b6c95a9fbf2cd48dd37e659
xuwei-k <6b656e6a69@gmail.com>
2014-10-18 23:59:54 +0900

    use inmap if fields size is one

--- src/main/scala/JsonLib.scala
+++ src/main/scala/JsonLib.scala
@@ -22,11 +22,19 @@ object JsonLib {
       s"""(__ \\ "$key").format[$tpe]"""
     }
 
-    def instance(clazz: CLAZZ): String = s"""
-  val ${clazz.className}Format: Format[${clazz.classNameUpper}] = (
+    def instance(clazz: CLAZZ): String = {
+      val valdef = s"""val ${clazz.className}Format: OFormat[${clazz.classNameUpper}]"""
+      val inmap = s"""${clazz.classNameUpper}.apply _, Function.unlift(${clazz.classNameUpper}.unapply)"""
+
+      if (clazz.fields.length == 1) {
+        s"""  $valdef = ${f(clazz.fields.head)}.inmap($inmap)"""
+      } else { s"""
+  $valdef = (
     ${clazz.fields.map(f).mkString(" and\n    ")}
-  )(${clazz.classNameUpper}.apply _, Function.unlift(${clazz.classNameUpper}.unapply))
+  )($inmap)
 """
+      }
+    }
   }
 
   val all: Set[JsonLib] = Set(Argonaut, Play)
@@ -35,7 +43,7 @@ object JsonLib {
   def objectDef(clazz: CLAZZ, libs: Set[JsonLib]): String = s"""
 object ${clazz.classNameUpper} {
 
-  ${libs.map(_.instance(clazz)).mkString("\n\n")}
+${libs.map(_.instance(clazz)).mkString("\n\n")}
 
 }
 """
